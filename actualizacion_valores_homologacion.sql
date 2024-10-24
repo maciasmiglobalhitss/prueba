84776a4c0603a6c02ed5bb091831e457e8653c9c
Miguel Macias <maciasmi@globalhitss.com>
2024-10-23 19:57:47 -0500

    Create actualizacion_valores_homologacion.sql

---
+++ actualizacion_valores_homologacion.sql
@@ -0,0 +1,58 @@
+
+select * from Menu
+
+select * from control_menu
+where 
+	
+id_menu=10 and 
+nombre_control='unitNetWeightBox'
+
+insert into control_menu(id_menu,orden,codigo,nombre_control,activo)
+values(10,25,'025','quaility',1)
+
+
+update control_menu set nombre_control='quantityCompound7Substitute'
+where id=242 and nombre_control='quantitySubstituteCompound7'
+
+update control_menu set nombre_control='producSubstituteCompound5'
+where id=235 and nombre_control='productSubstituteCompound5'
+
+
+
+update control_menu set nombre_control='producSubstituteCompound7'
+where id=241 and nombre_control='productSubstituteCompound7'
+
+
+update control_menu set nombre_control='producSubstituteCompound8'
+where id=244 and nombre_control='productSubstituteCompound8'
+
+update control_menu set nombre_control='producSubstituteCompound6'
+where id=238 and nombre_control='productSubstituteCompound6'
+
+update control_menu set nombre_control='producSubstituteCompound4'
+where id=232 and nombre_control='productSubstituteCompound4'
+
+
+
+update control_menu set nombre_control='producSubstituteCompound3'
+where id=229 and nombre_control='productSubstituteCompound3'
+
+
+update Menu set codigo_app_web= 2
+where id=2
+
+update Menu set codigo_app_web= 9
+where id=12
+
+update control_menu set nombre_control='catalogId'
+where id=1
+
+update Menu set codigo_app_web= 91
+where id=14
+
+
+update Menu set codigo_app_web= 92
+where id=16
+
+
+
